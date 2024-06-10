1e26b31645faf66c9d33cd54a75bb65623f81244
luzhipeng728 <luzhipeng728@126.com>
2024-06-10 21:19:27 +0800

    描述你的更改

--- relay/channel/openai/relay-openai.go
+++ relay/channel/openai/relay-openai.go
@@ -19,12 +19,23 @@ import (
 )
 
 var modelmapper = map[string]string{
-	"gpt-4-turbo":         "gpt-4-turbo-2024-04-09",
-	"gpt-4":               "gpt-4-0613",
-	"gpt-3.5-turbo":       "gpt-3.5-turbo-0125",
-	"gpt-3.5-turbo-16k":   "gpt-3.5-turbo-16k-0613",
-	"gpt-4-32k":           "gpt-4-32k-0613",
-	"gpt-4-turbo-preview": "gpt-4-0125-preview",
+	"gpt-3.5-turbo":          "gpt-3.5-turbo",
+	"gpt-3.5-turbo-0301":     "gpt-3.5-turbo",
+	"gpt-3.5-turbo-0613":     "gpt-3.5-turbo",
+	"gpt-3.5-turbo-1106":     "gpt-3.5-turbo",
+	"gpt-3.5-turbo-0125":     "gpt-3.5-turbo",
+	"gpt-3.5-turbo-16k-0613": "gpt-3.5-turbo-16k",
+	"gpt-4":                  "gpt-4",
+	"gpt-4-0314":             "gpt-4",
+	"gpt-4-0613":             "gpt-4",
+	"gpt-4-32k":              "gpt-4-32k",
+	"gpt-4-32k-0314":         "gpt-4-32k",
+	"gpt-4-32k-0613":         "gpt-4-32k",
+	"gpt-4-1106-preview":     "gpt-4",
+	"gpt-4-0125-preview":     "gpt-4",
+	"gpt-4-turbo-preview":    "gpt-4",
+	"gpt-4-turbo":            "gpt-4-turbo-2024-04-09",
+	"gpt-4-turbo-2024-04-09": "gpt-4-turbo-2024-04-09",
 }
 
 func OpenaiStreamHandler(c *gin.Context, resp *http.Response, relayMode int, model string) (*dto.OpenAIErrorWithStatusCode, string, int) {
